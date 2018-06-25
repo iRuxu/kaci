90c9d203be994242b021c4a6366699f5ebf8302b
iRuxu <rx6@qq.com>
2018-06-25 20:27:17 +0800

    modified the file create method

--- bin/build.js
+++
@@ -1,6 +0,0 @@
-const shell = require('shelljs');
-const version = require('../package.json').version
-
-shell.exec(`git tag ${version}`)
-shell.exec(`git push origin ${version}`)
-shell.exec('npm publish')
